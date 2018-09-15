7fd8534f891b710ebc144d1bc4a0f18080b73962
anton-mladenov <tonkata@mail.com>
2018-09-15 22:23:00 +0200

    serving html css and js files straight from the server

--- testing/server.js
+++ testing/server.js
@@ -5,19 +5,29 @@ const hostname = "127.0.0.1"
 const port = 3001
 let time = `${new Date().getHours()}:${new Date().getMinutes()}`
 
-fs.readFile("./index.html", (err, html) => {
-	if (err) {
-		throw err
+const server = http.createServer((req, res) => {
+
+	if (req.url === "/styles.css") {
+		res.writeHead(200, { "Content-Type": "text/css" })
+		let cssFile = fs.readFileSync("./styles.css", { encoding: 'utf8' })
+		res.end(cssFile)
 	}
-	const server = http.createServer((req, res) => {
+
+	if (req.url === "/") {
 		res.writeHead(200, { "Content-Type": "text/html" })
-		res.write(html)
-		res.end()
-	})
-	server.listen(port, hostname, () => {
-		console.log(`Server running at http://${hostname}:${port}/ in ${time}`)
-	})
+		let htmlFile = fs.readFileSync("./index.html", { encoding: 'utf8' })
+		res.end(htmlFile)
+	}
+
+	if (req.url === "/main.js") {
+		res.writeHead(200, { "Content-Type": "text/javascript" })
+		let jsFile = fs.readFileSync("./main.js", { encoding: 'utf8' })
+		res.end(jsFile)
+	}
+
+	res.end()
 })
 
-
-
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/ in ${time}`)
+})
